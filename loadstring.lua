7cd5d0443600befa9e746765f44ff14612c56b6d
02Dcs <95237577+02Dcs@users.noreply.github.com>
2023-05-28 22:09:56 +0200

    Update loadstring.lua

--- loadstring.lua
+++ loadstring.lua
@@ -1,4 +1,5 @@
 --Best bypass ong! made by 02
 --Information u cant say nigger,dick
 --Otherwise all words are supported!
+--Max bad words 2-3!
 loadstring(game:HttpGet("https://raw.githubusercontent.com/02Dcs/Chat/main/ChatByp.lua", true))()
