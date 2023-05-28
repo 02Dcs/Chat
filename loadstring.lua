7b5fd92d24b5f3dd4d4ccce09f5a8b56d631afd4
02Dcs <95237577+02Dcs@users.noreply.github.com>
2023-05-28 22:16:53 +0200

    Update loadstring.lua

--- loadstring.lua
+++ loadstring.lua
@@ -3,3 +3,10 @@
 --Otherwise all words are supported!
 --Max bad words 2-3!
 loadstring(game:HttpGet("https://raw.githubusercontent.com/02Dcs/Chat/main/ChatByp.lua", true))()
+
+--ChatByp2
+--Max words 2!
+--Support all words!
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/02Dcs/Chat/main/ChatBypV2", true))()
+
