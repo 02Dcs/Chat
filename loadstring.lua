e18596fb5f4f3dbb215438f45fe543067f216be2
02Dcs <95237577+02Dcs@users.noreply.github.com>
2023-05-28 22:32:32 +0200

    Update loadstring.lua

--- loadstring.lua
+++ loadstring.lua
@@ -8,15 +8,17 @@
                                                                            
 ]]
 
+getgenv().ChatFilterMode = 1;
+
+if ChatFilterMode == 1 then
 --[[Best bypass ong! made by 02
 Information u cant say nigger,dick
 Otherwise all words are supported!
 Max bad words 2-3!]]
 loadstring(game:HttpGet("https://raw.githubusercontent.com/02Dcs/Chat/main/ChatByp.lua", true))()
-
+elseif ChatFilterMode == 2 then 
 --[[ChatByp2
 Max words 2!
 Support all words!]]
-
 loadstring(game:HttpGet("https://raw.githubusercontent.com/02Dcs/Chat/main/ChatBypV2", true))()
-
+end
