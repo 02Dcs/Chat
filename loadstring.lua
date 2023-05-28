d1201d9b138f900dafc8d4f4c6692514afe09bfc
02Dcs <95237577+02Dcs@users.noreply.github.com>
2023-05-28 22:29:32 +0200

    Update loadstring.lua

--- loadstring.lua
+++ loadstring.lua
@@ -1,12 +1,22 @@
---Best bypass ong! made by 02
---Information u cant say nigger,dick
---Otherwise all words are supported!
---Max bad words 2-3!
+--[[ 
+ ██████╗ ██████╗  ██████╗██╗  ██╗ █████╗ ████████╗██████╗ ██╗   ██╗██████╗ 
+██╔═████╗╚════██╗██╔════╝██║  ██║██╔══██╗╚══██╔══╝██╔══██╗╚██╗ ██╔╝██╔══██╗
+██║██╔██║ █████╔╝██║     ███████║███████║   ██║   ██████╔╝ ╚████╔╝ ██████╔╝
+████╔╝██║██╔═══╝ ██║     ██╔══██║██╔══██║   ██║   ██╔══██╗  ╚██╔╝  ██╔═══╝ 
+╚██████╔╝███████╗╚██████╗██║  ██║██║  ██║   ██║   ██████╔╝   ██║   ██║     
+ ╚═════╝ ╚══════╝ ╚═════╝╚═╝  ╚═╝╚═╝  ╚═╝   ╚═╝   ╚═════╝    ╚═╝   ╚═╝     
+                                                                           
+]]
+
+--[[Best bypass ong! made by 02
+Information u cant say nigger,dick
+Otherwise all words are supported!
+Max bad words 2-3!]]
 loadstring(game:HttpGet("https://raw.githubusercontent.com/02Dcs/Chat/main/ChatByp.lua", true))()
 
---ChatByp2
---Max words 2!
---Support all words!
+--[[ChatByp2
+Max words 2!
+Support all words!]]
 
 loadstring(game:HttpGet("https://raw.githubusercontent.com/02Dcs/Chat/main/ChatBypV2", true))()
 
