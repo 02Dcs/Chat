f5774f056216e25fad73032be9b213b0121672c0
02Dcs <95237577+02Dcs@users.noreply.github.com>
2023-06-26 12:24:36 +0200

    Update ChatByp.lua

--- ChatByp.lua
+++ ChatByp.lua
@@ -1,20 +1,4 @@
-repeat task.wait(0.2) until game:IsLoaded()
-
-local Bypasses = {
-        [" "] = "\226\129\165\226\129\165\226\129\165\226\129\165\226\129\165\226\129\165 \226\129\165\226\129\165\226" ..
-            "\129\165\226\129\165\226\129\165\226\129\165",
-
-           ["a"] = "а",
-           ["s"] = "ѕ",
-           ["o"] = "о",
-           ["o"] = "о",
-           ["c"] = "с",
-           ["p"] = "р",
-    }
-
-print('Symbols!');
-
-local Alphabet,old = loadstring(game:HttpGet("https://raw.githubusercontent.com/02Dcs/Chat/main/Symbols"))()
+print("hi") local Alphabet,old = loadstring(game:HttpGet("https://raw.githubusercontent.com/02Dcs/Chat/main/Symbols"))()
 old = hookmetamethod(game,"__namecall",function(self,...)
     if getnamecallmethod() == "FireServer" and tostring(self) == "SayMessageRequest" then
         local Arguments = {...}
@@ -27,12 +11,26 @@ old = hookmetamethod(game,"__namecall",function(self,...)
         end
 
       local new = Message
-      Arguments[1] = "̌ 𐌜⬜⬜⬜⬜ť   𐌜⬜▰◻   ₠̴̻̆ 𐌜⬜▰◻ 𐌜⬜▰◻     ͣ₠̴̻̆ 𐌜⬜▰◻  ͣ 𐌖￭⬜▰◻   ₠̴̻̆ 𐌜⬜▰◻ 𐌜⬜▰◻     ͣ₠̴̻̆ 𐌜⬜▰◻ 𐌖 ̌ 𐌜⬜#̌  ̌𐌖⬜𐌖⬜ ̌  𐌜⬜̌ 𐌖 ̌  ̌𐌖⬛ 𐌜⬜" .. new
+      Arguments[1] = "̌ 𐌜⬜⬜⬜⬜̌   𐌜⬜ 𐌜▰◻ ˜ˆ 𐌔˜𐌖 𐌖￭𐌒▰𐌖𐌖  ॓॓॓॓॓॓॓॓॓॓॓॓॓॓॓॓॓॓॓॓॓॓॓॓॓॓॓॓॓॓॓॓॓॓॓॓॓॓॓॓॓॓॓॓॓▰𐌖𐌖  𐌜  𐌖￭𐌒▰𐌖𐌖   ̌  ̌𐌖 𐌜⬜𐌖 𐌜¹¹¹¹¡ 𐌜⬜𐌜⬜ 𐌖¹¹¹¹¡  𐌜⬜⬜⬜॓॓॓॓i 𐌜⬜⬜ ť" .. new
 
       return old(self, unpack(Arguments))
     end
     return old(self, ...)
 end)
+
+    local Bypasses = {
+        [" "] = "\226\129\165\226\129\165\226\129\165\226\129\165\226\129\165\226\129\165 \226\129\165\226\129\165\226" ..
+            "\129\165\226\129\165\226\129\165\226\129\165",
+
+           ["a"] = "а",
+           ["s"] = "ѕ",
+           ["o"] = "о",
+           ["e"] = "е",
+           ["o"] = "о",
+           ["c"] = "с",
+           ["p"] = "р",
+    }
+
 
 local ReplicatedStorage = game:GetService("ReplicatedStorage")
     local Remote = ReplicatedStorage:FindFirstChild("SayMessageRequest", true)
