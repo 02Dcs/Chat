c213196ca0cbdef9a05a43119fe15410794ec66e
02Dcs <95237577+02Dcs@users.noreply.github.com>
2023-12-02 23:00:31 +0100

    Create chatbypass.lua
    join :)

---
+++ chatbypass.lua
@@ -0,0 +1,8 @@
+Best roblox chat bypass - 2023/2024
+
+[+] Apply your own chat bypass configuration
+[+] Automatic HWID changer upon ban.
+[+] Bypasses all words.
+
+Join today for daily bot updates and improvements!
+https://discord.gg/ejFqne6tZb 
